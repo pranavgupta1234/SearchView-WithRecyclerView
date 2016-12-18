f5642d08999777191d8c301a7d0c5ce56e2ef527
pranavgupta1234 <pranavgupta4321@gmail.com>
2016-12-18 13:41:58 +0530

    first commit

---
+++ RecyclerViewFilter/app/src/main/java/com/tutorialsbuzz/recyclerview/TabFragments/ItemViewHolder.java
@@ -0,0 +1,30 @@
+package com.tutorialsbuzz.recyclerview.TabFragments;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.TextView;
+import com.tutorialsbuzz.recyclerview.R;
+
+public class ItemViewHolder extends RecyclerView.ViewHolder {
+
+    public TextView name_TextView;
+    public TextView iso_TextView;
+
+
+
+    public ItemViewHolder(View itemView) {
+        super(itemView);
+        itemView.setClickable(true);
+        name_TextView = (TextView) itemView.findViewById(R.id.country_name);
+        iso_TextView = (TextView) itemView.findViewById(R.id.country_iso);
+
+    }
+
+    public void bind(CountryModel countryModel) {
+        name_TextView.setText(countryModel.getName());
+        iso_TextView.setText(countryModel.getisoCode());
+
+    }
+
+
+}
